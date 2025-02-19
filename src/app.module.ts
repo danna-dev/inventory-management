bc33eabde634ef071a8d9b61ff1eb5d3c8d1ee27
Daniela <jdgranados@hotmail.es>
2025-02-18 23:15:46 -0500

    Chore: config env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
-  imports: []
+  imports: [ 
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema,
+    }),
+  ]
 })
 export class AppModule {}
