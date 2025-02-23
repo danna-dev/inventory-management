8fdb2005399045d9c197e4fbd74d76d79772d58d
Daniela <jdgranados@hotmail.es>
2025-02-23 15:52:27 -0500

    db config

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,5 +2,9 @@ import * as JOI from 'joi';
 
 export const JoiValidationSchema = JOI.object({
   PORT: JOI.number().default(3005),
-  ENVIRONMENT: JOI.string().default('dev'),
+  DB_HOST: JOI.string().required(),
+  DB_PORT: JOI.number().required(),
+  DB_USERNAME: JOI.string().required(),
+  DB_PASSWORD: JOI.string().required(),
+  DB_NAME: JOI.string().required(),
 });
