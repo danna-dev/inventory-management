a210630b10868e4a86a19bc5c2317dbff92f22c8
Daniela <jdgranados@hotmail.es>
2025-02-23 15:52:27 -0500

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ 
     ConfigModule.forRoot({
       load: [ EnvConfiguration ],
       validationSchema: JoiValidationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres' ,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     }),
   ]
 })
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,10 @@
 export const EnvConfiguration = () => ({
-    environment: process.env.NODE_ENV,
-    port: +process.env.PORT ,
+    port: +process.env.PORT,
+    db: {
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        name: process.env.DB_NAME,
+    }
 });
